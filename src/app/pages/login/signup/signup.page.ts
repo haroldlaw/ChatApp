8cdfb217ba34ca5d766da6bb0aa3341198d5eb89
haroldlaw <g00438467@atu.ie>
2025-04-15 16:52:39 +0100

    login page design done

--- src/app/pages/login/signup/signup.page.ts
+++ src/app/pages/login/signup/signup.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
