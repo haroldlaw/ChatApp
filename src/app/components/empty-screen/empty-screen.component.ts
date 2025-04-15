248e3ce59a79f82cd1e8a7313c4e337cb31a15a4
haroldlaw <g00438467@atu.ie>
2025-04-15 16:14:56 +0100

    empty screen design done

--- src/app/components/empty-screen/empty-screen.component.ts
+++ src/app/components/empty-screen/empty-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   standalone: false,
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empty-screen.component.scss'],
 })
 export class EmptyScreenComponent  implements OnInit {
+  @Input() model: any
 
   constructor() { }
 
