8a2b0b71355a338170ae2a77939aeab1d59eefc5
haroldlaw <g00438467@atu.ie>
2025-04-15 14:27:33 +0100

    home page design completed

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ModalController, PopoverController } from '@ionic/angular';
 
 @Component({
   standalone: false,
@@ -7,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  @ViewChild('new_chat') modal: ModalController
+  @ViewChild('popover') popover: PopoverController
+  segment = 'chats'
+  open_new_chat = false
+  users = [
+    { id: 1, name: 'Darren', photo: 'https://i.pravatar.cc/300' },
+    { id: 2, name: 'Sam', photo: 'https://i.pravatar.cc/305' }
+  ]
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  logout() {
+    this.popover.dismiss()
+  }
+  onSegmentChanged(event: any) {
+    console.log(event)
+  }
+
+
+  newChat() {
+    this.open_new_chat = true
+  }
+
+  onWillDismiss(event: any) {
+
+  }
+
+  cancel() {
+    this.modal.dismiss();
+    this.open_new_chat = false;
+  }
+
+  startChat(item) {
+
+  }
 }
