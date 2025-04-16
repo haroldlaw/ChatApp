7c9564c39d44acdb780e0c9f207e22361f0fcdba
haroldlaw <g00438467@atu.ie>
2025-04-16 13:45:42 +0100

    login feature implemented with authentication

--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -1,5 +1,34 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanMatch, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanMatch {
+
+  constructor(
+    private authService: AuthService, 
+    private router: Router) {}
+
+  async canMatch(): Promise<boolean> {
+      try {
+        const user = await this.authService.checkAuth();
+        console.log(user);
+        if(user) {
+          return true;
+        } else {
+          this.navigate('/login');
+          return false;
+        }
+      } catch(e) {
+        console.log(e);
+        this.navigate('/login');
+        return false;
+      }
+  }
+
+  navigate(url) {
+    this.router.navigateByUrl(url, {replaceUrl: true});
+  }
+}
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   standalone: false,
@@ -12,19 +15,23 @@ export class LoginPage implements OnInit {
   isTypePassword: boolean = true
   isLogin = false
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private alertController: AlertController
+  ) {
     this.initForm()
-   }
+  }
 
-  ngOnInit():void {}
+  ngOnInit(): void { }
 
   initForm() {
     this.form = new FormGroup({
-      email: new FormControl('', 
-        {validators: [Validators.required, Validators.email]}
+      email: new FormControl('',
+        { validators: [Validators.required, Validators.email] }
       ),
-      password: new FormControl('', 
-        {validators: [Validators.required, Validators.minLength(8)]}
+      password: new FormControl('',
+        { validators: [Validators.required, Validators.minLength(8)] }
       ),
     });
   }
@@ -34,9 +41,34 @@ export class LoginPage implements OnInit {
   }
 
   onSubmit() {
-    if(!this.form.valid) return;
+    if (!this.form.valid) return;
     console.log(this.form.value);
-    //this.login(this.form);
+    this.login(this.form);
+  }
+
+  login(form) {
+    this.authService.login(form.value.email, form.value.password).then(data => {
+      console.log(data);
+      this.router.navigateByUrl('/home');
+      form.reset();
+    })
+      .catch(e => {
+        console.log(e);
+        let msg: string = 'Log in failed. Please try again.';
+        if (e.code == 'auth/user-not-found') msg = 'Email address could not be found';
+        else if (e.code == 'auth/wrong-password') msg = 'Please enter a correct password';
+        this.showAlert(msg);
+      });
+  }
+
+  async showAlert(msg) {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      message: msg,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
   }
 
 }
