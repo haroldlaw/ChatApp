a4b65a4f0a10990ec028ffb1e921c593396a2ff2
haroldlaw <g00438467@atu.ie>
2025-04-16 12:09:43 +0100

    sign up page completed

--- src/app/pages/login/signup/signup.page.ts
+++ src/app/pages/login/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   standalone: false,
@@ -7,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  signupForm: FormGroup
+  isTypePassword: boolean = true
+  isLoading: boolean = false
 
-  constructor() { }
+  constructor() { 
+    this.initForm()
+  }
 
   ngOnInit() {
+  }
+
+  initForm() {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', 
+        {validators: [Validators.required]}
+      ),
+      email: new FormControl('', 
+        {validators: [Validators.required, Validators.email]}
+      ),
+      password: new FormControl('', 
+        {validators: [Validators.required, Validators.minLength(8)]}
+      ),
+    });
+  }
+
+  onChange() {
+    this.isTypePassword = !this.isTypePassword;
+  }
+
+  onSubmit() {
+    if(!this.signupForm.valid) return;
+    console.log(this.signupForm.value);
+    //this.register(this.signupForm);
   }
 
 }
