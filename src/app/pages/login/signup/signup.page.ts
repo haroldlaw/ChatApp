4b0cd09e850f84c8e9885455d687afa71de396eb
haroldlaw <g00438467@atu.ie>
2025-04-16 13:28:54 +0100

    sign up page fuctionality completed

--- src/app/pages/login/signup/signup.page.ts
+++ src/app/pages/login/signup/signup.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from './../../../services/auth/auth.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   standalone: false,
@@ -12,7 +15,10 @@ export class SignupPage implements OnInit {
   isTypePassword: boolean = true
   isLoading: boolean = false
 
-  constructor() { 
+  constructor(private router: Router,
+    private authService: AuthService,
+    private alertController: AlertController
+  ) {
     this.initForm()
   }
 
@@ -21,14 +27,14 @@ export class SignupPage implements OnInit {
 
   initForm() {
     this.signupForm = new FormGroup({
-      username: new FormControl('', 
-        {validators: [Validators.required]}
+      username: new FormControl('',
+        { validators: [Validators.required] }
       ),
-      email: new FormControl('', 
-        {validators: [Validators.required, Validators.email]}
+      email: new FormControl('',
+        { validators: [Validators.required, Validators.email] }
       ),
-      password: new FormControl('', 
-        {validators: [Validators.required, Validators.minLength(8)]}
+      password: new FormControl('',
+        { validators: [Validators.required, Validators.minLength(8)] }
       ),
     });
   }
@@ -38,9 +44,38 @@ export class SignupPage implements OnInit {
   }
 
   onSubmit() {
-    if(!this.signupForm.valid) return;
+    if (!this.signupForm.valid) return;
     console.log(this.signupForm.value);
-    //this.register(this.signupForm);
+    this.register(this.signupForm);
   }
 
+  register(form) {
+    this.isLoading = true;
+    console.log(form.value);
+    this.authService.register(form.value).then((data: any) => {
+      console.log(data);
+      this.router.navigateByUrl('/home');
+      this.isLoading = false;
+      form.reset();
+    })
+      .catch(e => {
+        console.log(e);
+        this.isLoading = false;
+        let msg: string = 'Sign up failed. Please try again.';
+        if (e.code == 'auth/email-already-exist') {
+          msg = 'Email already exist';
+        }
+        this.showAlert(msg);
+      });
+  }
+
+  async showAlert(msg) {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      message: msg,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 }
